c034615288f07c6f1a1dcddf6aa9175869da7249
Daniel Civit <daniel.civit@multisafepay.com>
2021-09-21 19:11:19 +0200

    PRES-125: Prevent bypassing terms & conditions during checkout by pressing enter (#76)

--- views/js/front.js
+++ views/js/front.js
@@ -20,7 +20,18 @@
  */
 
 $(document).ready(function () {
+
     $("[id^='multisafepay-form-']").each(function () {
         $(this).parent().closest("div").prev("div").find("label img").css('height', '30px');
     });
+
+
+    $("[id^='multisafepay-form-']").keypress(
+        function (event) {
+            if (event.which === 13) {
+                event.preventDefault();
+            }
+        }
+    );
+    
 });
