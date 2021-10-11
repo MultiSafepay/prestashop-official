0793d72006ee82fb4252d63bf05fde1e82909fef
Daniel Civit <daniel.civit@multisafepay.com>
2021-10-11 12:09:56 +0200

    PRES-160: Add in3 PaymentOption (#139)
    * PRES-160: Add in3 PaymentOption
    
    * PRES-160: Add in3 PaymentOption
    
    Co-authored-by: Jasper Nadi <26003384+Jasper-MultiSafepay@users.noreply.github.com>

---
+++ src/PaymentOptions/PaymentMethods/In3.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace MultiSafepay\PrestaShop\PaymentOptions\PaymentMethods;
+
+use MultiSafepay\Api\Transactions\OrderRequest\Arguments\GatewayInfoInterface;
+use MultiSafepay\PrestaShop\PaymentOptions\Base\BasePaymentOption;
+use MultiSafepay\PrestaShop\PaymentOptions\Base\BaseGatewayInfo;
+use MultiSafepay\Api\Transactions\OrderRequest\Arguments\GatewayInfo\Meta;
+use Tools;
+use Order;
+use Address;
+use Context;
+
+class In3 extends BasePaymentOption
+{
+    protected $name = 'in3';
+    protected $gatewayCode = 'IN3';
+    protected $logo = 'in3.png';
+    protected $hasConfigurableDirect = true;
+    protected $canProcessRefunds = false;
+
+    public function getTransactionType(): string
+    {
+        $checkoutVars = Tools::getAllValues();
+        return (empty($checkoutVars['gender']) || empty($checkoutVars['birthday'])) ? self::REDIRECT_TYPE : self::DIRECT_TYPE;
+    }
+
+    public function getGatewaySettings(): array
+    {
+        $options = parent::getGatewaySettings();
+
+        $options['MULTISAFEPAY_MIN_AMOUNT_'.$this->getUniqueName()]['default'] = '100';
+        $options['MULTISAFEPAY_MAX_AMOUNT_'.$this->getUniqueName()]['default'] = '3000';
+
+        return $options;
+    }
+
+    public function getDirectTransactionInputFields(): array
+    {
+        return [
+            [
+                'type'          => 'select',
+                'name'          => 'gender',
+                'placeholder'   => $this->module->l('Salutation'),
+                'options'       => [
+                    [
+                        'value' => 'mr',
+                        'name'  => 'Mr.',
+                    ],
+                    [
+                        'value' => 'mrs',
+                        'name'  => 'Mrs.',
+                    ],
+                    [
+                        'value' => 'miss',
+                        'name'  => 'Miss',
+                    ]
+                ],
+            ],
+            [
+                'type'          => 'date',
+                'name'          => 'birthday',
+                'placeholder'   => $this->module->l('Birthday'),
+                'value'         => Context::getContext()->customer->birthday ?? '',
+            ]
+        ];
+    }
+
+    public function getGatewayInfo(Order $order, array $data = []): GatewayInfoInterface
+    {
+        if (empty($data['gender']) && empty($data['birthday'])) {
+            return new BaseGatewayInfo();
+        }
+
+        $gatewayInfo = new Meta();
+        $gatewayInfo->addPhoneAsString((new Address($order->id_address_invoice))->phone);
+        if (!empty($data['gender'])) {
+            $gatewayInfo->addGenderAsString($data['gender']);
+        }
+        if (!empty($data['birthday'])) {
+            $gatewayInfo->addBirthdayAsString($data['birthday']);
+        }
+
+        return $gatewayInfo;
+    }
+}
