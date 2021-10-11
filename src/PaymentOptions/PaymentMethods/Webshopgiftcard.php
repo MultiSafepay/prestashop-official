41d9aa72d76275846d933f4d82e32c5ac62067d2
Daniel Civit <daniel.civit@multisafepay.com>
2021-10-11 11:00:21 +0200

    PRES-162: Add Webshop gift card PaymentOption (#141)
    * PRES-162: Add Webshop gift card PaymentOption
    
    * PRES-162: Add PHPUnit test to check if possible process refunds
    
    * PRES-162: Add PHPUnit test to check if possible process refunds
    
    * PRES-162: Add PHPUnit test to check if possible process refunds

---
+++ src/PaymentOptions/PaymentMethods/Webshopgiftcard.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace MultiSafepay\PrestaShop\PaymentOptions\PaymentMethods;
+
+use MultiSafepay\PrestaShop\PaymentOptions\Base\BasePaymentOption;
+
+class Webshopgiftcard extends BasePaymentOption
+{
+    protected $name = 'Webshop gift card';
+    protected $gatewayCode = 'WEBSHOPGIFTCARD';
+    protected $logo = 'webshopgiftcard.png';
+    protected $canProcessRefunds = false;
+}
